50591829492e3babb251c5dad151d2e78beaf704
grandmasterspoc <dilloncortez@gmail.com>
2015-05-04 11:17:52 -0600

    Added AI to play game for you

--- driver.rb
+++ driver.rb
@@ -4,8 +4,8 @@ class CubeRunner
     @life = true
     @boardwidth = length
     @boardheight = length
-    @blocks = "[] "
-    @field =  "   "
+    @blocks = "[]"
+    @field =  "  "
     @board = Array.new(@boardheight) {Array.new (@boardwidth) {@field}} 
     @start_position = (@boardheight-1)
     @row_pos = length / 2
@@ -21,13 +21,12 @@ class CubeRunner
     end    
   end
 
-
   def run
-    @board.unshift(Array.new(@boardwidth) { rand(15) == 1 ? @blocks : @field})
+    @board.unshift(Array.new(@boardwidth) { rand(@row_pos * 0.75) == 1 ? @blocks : @field})
     @board.pop
   end 
 
-  def position(pos)
+  def detect_colision(pos)
     @position = @start_position
     if @board[@position][pos] == @blocks
       @life =  false
@@ -35,16 +34,39 @@ class CubeRunner
     @board[@position][pos] = @player
   end 
 
+  def move_left(pos)
+    @board[@start_position][pos], @board[@start_position][pos-1] = @board[@start_position][pos-1], @board[@start_position][pos]
+    @row_pos = (pos-1)
+  end  
+
+  def move_right(pos)
+    @board[@start_position][pos], @board[@start_position][pos+1] = @board[@start_position][pos+1], @board[@start_position][pos]
+    @row_pos = (pos+1)
+  end
+
+  def move(pos)
+    if @board[@start_position-1][pos] == @blocks
+      if @board[@start_position-1][pos-1] == @blocks || @board[@start_position][pos-1] == @blocks
+        move_right(pos)
+      elsif @board[@start_position-1][pos+1] == @blocks || @board[@start_position][pos+1] == @blocks
+        move_left(pos)
+      else 
+        rand(2) == 1 ? move_right(pos) : move_left(pos) 
+      end   
+    end  
+    detect_colision(pos)
+  end 
+   
 
   def play 
-    while @life == true
+    while @life == true     
+      move(@row_pos)
       draw
       run
-      position(@row_pos)
       sleep 0.05
     end
   end
 end
 
-game = CubeRunner.new(50)
-game.play 
+game = CubeRunner.new(30)
+puts game.play
