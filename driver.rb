30b4ed7128631b09c5171b4b8f8e4790e514e566
grandmasterspoc <dilloncortez@gmail.com>
2015-05-03 18:58:16 -0600

    Made game a little more object oriented

--- driver.rb
+++ driver.rb
@@ -1,45 +1,50 @@
-@player = '/\\'
-@life = true
-@boardwidth = 30
-@boardheight = 30
-@blocks = "[]"
-@field =  "  "
-@board = Array.new(@boardheight) {Array.new (@boardwidth) {@field}} 
-@start_position = (@boardheight-1)
+class CubeRunner
+  def initialize(boardwidth, boardheight)
+    @player = '/\\'
+    @life = true
+    @boardwidth = boardwidth
+    @boardheight = boardheight
+    @blocks = "[]"
+    @field =  "  "
+    @board = Array.new(@boardheight) {Array.new (@boardwidth) {@field}} 
+    @start_position = (@boardheight-1)
+  end
 
-def draw
-  system 'clear' or system 'cls'
-  @board.each_with_index do |cord, x|
-    cord.each_with_index do |pos, y|
-      print "#{@board[x][y]}"
+  def draw
+    system 'clear' or system 'cls'
+    @board.each_with_index do |cord, x|
+      cord.each_with_index do |pos, y|
+        print "#{@board[x][y]}"
+      end
+      puts ""
+    end    
+  end
+
+
+  def run
+    @board.unshift(Array.new(@boardwidth) { rand(15) == 1 ? @blocks : @field})
+    @board.pop
+  end 
+
+  def position(pos)
+    @position = @start_position
+    if @board[@position][pos] == @blocks
+      @life =  false
+    end  
+    @board[@position][pos] = @player
+  end 
+
+
+  def play 
+    while @life == true
+      draw
+      run
+      position(15)
+      sleep 0.05
     end
-    puts ""
-  end    
-end
-
-
-def run
-  @board.unshift(Array.new(@boardwidth) { rand(15) == 1 ? @blocks : @field})
-  @board.pop
-end 
-
-def position(pos)
-  @position = @start_position
-  if @board[@position][pos] == @blocks
-    @life =  false
-  end  
-  @board[@position][pos] = @player
-end 
-
-
-def play 
-  while @life == true
-    draw
-    run
-    position(15)
-    sleep 0.05
   end
 end
 
+game = CubeRunner.new(30, 30)
 
-play  
+game.play 
