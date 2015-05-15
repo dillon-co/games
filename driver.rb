8c829050f39ffa1c9af76a669d253a30ce25ecc6
grandmasterspoc <dilloncortez@gmail.com>
2015-05-15 14:45:33 -0600

    Added new classes, More refactoring needed, but this is a start. Life bar not working yet.

--- driver.rb
+++ driver.rb
@@ -1,14 +1,68 @@
-class CubeRunner
+class Player
+  def initialize(number_of_lives=10)
+    @lives = number_of_lives
+  end
+
+  def alive?
+    @lives > 0
+  end
+
+  def hit
+    @lives -= 1
+  end
+
+  def image
+    '/\\'
+  end
+
+  def life_bar_image
+    hit
+    bar = "=" * @lives
+    @bar_array = bar.split('')
+  end  
+
+  def life_bar_array
+    @bar_array
+  end  
+end
+
+class Board
   def initialize(length)
-    @player = '/\\'
-    @life = true
-    @boardwidth = length
-    @boardheight = length
     @blocks = "[]"
     @field =  "  "
-    @board = Array.new(@boardheight) {Array.new (@boardwidth) {@field}} 
-    @start_position = (@boardheight-1)
+    @boardwidth = length
+    @boardheight = length 
+    @board = Array.new(length) {Array.new (length) {@field}}
+  end
+
+  def board
+    @board
+  end 
+
+  def blocks
+    @blocks
+  end
+  
+  def field
+    @field
+  end
+
+  def life_bar
+    ::Player.new.life_bar_array.to_a.each_with_index do |health, index|
+      @board[1][index] = health
+    end  
+  end  
+end    
+
+class CubeRunner
+  def initialize(length, speed)
+    @length = length
+    @player = ::Player.new(2)
+    @board_items = ::Board.new(length)
+    @board = @board_items.board
+    @start_position = (length-1)
     @row_pos = length / 2
+    @speed = speed
   end
 
   def draw
@@ -18,26 +72,27 @@ class CubeRunner
         print "#{@board[x][y]}"
       end
       puts ""
-    end    
+    end
   end
 
   def run
-    @board.unshift(Array.new(@boardwidth) { rand(15) == 1 ? @blocks : @field})
+    @board.unshift(Array.new(@length) { rand(15) == 1 ? @board_items.blocks : @board_items.field})
     @board.pop
-  end 
+  end
 
   def detect_colision(pos)
     @position = @start_position
-    if @board[@position][pos] == @blocks
-      @life =  false
-    end  
-    @board[@position][pos] = @player
-  end 
+    if @board[@position][pos] == @board_items.blocks
+      @player.hit
+    end
+    @board[@position][pos] = @player.image
+    @board_items.life_bar
+  end
 
   def move_left(pos)
     @board[@start_position][pos], @board[@start_position][pos-1] = @board[@start_position][pos-1], @board[@start_position][pos]
     @row_pos = (pos-1)
-  end  
+  end
 
   def move_right(pos)
     @board[@start_position][pos], @board[@start_position][pos+1] = @board[@start_position][pos+1], @board[@start_position][pos]
@@ -45,31 +100,30 @@ class CubeRunner
   end
 
   def move(pos)
-    if @board[@start_position-2][pos] == @blocks && (@board[@start_position-2][pos+1] == @blocks || @board[@start_position-2][pos-1] == @blocks)
+    if @board[@start_position-2][pos] == @board_items.blocks && (@board[@start_position-2][pos+1] == @board_items.blocks || @board[@start_position-2][pos-1] == @blocks)
       rand(2) == 1 ? move_right(pos) : move_left(pos)
     end
-    if @board[@start_position-1][pos] == @blocks
-      if @board[@start_position-1][pos-1] == @blocks || @board[@start_position][pos-1] == @blocks
+    if @board[@start_position-1][pos] == @board_items.blocks
+      if @board[@start_position-1][pos-1] == @board_items.blocks || @board[@start_position][pos-1] == @board_items.blocks
         move_right(pos)
-      elsif @board[@start_position-1][pos+1] == @blocks || @board[@start_position][pos+1] == @blocks
+      elsif @board[@start_position-1][pos+1] == @board_items.blocks || @board[@start_position][pos+1] == @board_items.blocks
         move_left(pos)
-      else 
-        rand(2) == 1 ? move_right(pos) : move_left(pos) 
-      end   
-    end  
+      else
+        rand(2) == 1 ? move_right(pos) : move_left(pos)
+      end
+    end
     detect_colision(pos)
-  end 
+  end
 
-
-  def play 
-    while @life == true     
+  def play
+    while @player.alive?
       move(@row_pos)
       draw
       run
-      sleep 0.05
+      sleep (@speed)
     end
   end
 end
 
-game = CubeRunner.new(30)
+game = CubeRunner.new(30, 0.03)
 puts game.play
