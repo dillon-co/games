a9a20631ed98b32b7123a9c28f118ce672570fd9
grandmasterspoc <dilloncortez@gmail.com>
2016-01-28 00:07:01 -0700

    Initial commit. About to use threads to draw board :D

---
+++ frogger.rb
@@ -0,0 +1,73 @@
+class Lane
+  def initialize
+    @log = "=="
+    @car = "%%"
+    @space = "  "
+    @safe_lane = Array.new(@size*2) {"__"}
+    @lane = create_row(6, @car)
+    @river = create_row(4, @log)
+  end  
+  
+  def tick_row(row, object, frequency, speed)
+    row.unshift(rand(frequency) == 1 ? object : space)
+    row.pop
+    sleep speed
+  end    
+end  
+
+
+class Board
+  attr_accessor :log, :car, :space, :lane, :river, :board, :safe_lane, :size
+  def initialize(size)
+    @size = size
+    @board = create_board
+  end  
+
+  def create_row(frequency, object)
+    Array.new(size*2) {rand(frequency) == 1 ? object : space}
+  end  
+
+  def create_board
+    Array.new(@size) {
+    if rand(3) == 1
+      rand(2) == 1 ? lane : river
+    else
+      safe_lane
+    end  
+    } 
+  end  
+
+  def draw_board
+    board.each_with_index do |row, y|
+      row.each_with_index do |col, x|
+        print "#{board[y][x]}"
+      end
+      puts ""  
+    end  
+    puts "LIFE BAR GOES HERE"
+  end
+
+  def tick_game(frequency1, frequency2)
+  end  
+
+end  
+
+class Character
+  attr_accessor :image
+  def initialize
+    @image = "πø"
+  end 
+end  
+
+b = Board.new(20)
+
+
+def play(game)
+  while true
+    game.draw_board
+    game.tick_game(3, 4)
+    puts 
+  end
+end  
+
+play(b)
