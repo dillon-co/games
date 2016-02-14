6c6ba1222e079de128cb52e4701fd684cdb685c4
grandmasterspoc <dilloncortez@gmail.com>
2016-02-14 03:41:31 -0700

    fixed drawing of rivers and lanes. Still hacky though :)

--- frogger.rb
+++ frogger.rb
@@ -1,75 +1,114 @@
 class Lane
-  attr_accessor :log, :car, :space, :lane, :river 
-  def initialize
-    @log = "=="
-    @car = "%%"
-    @space = "  "
-    @safe_lane = Array.new(@size*2) {"__"}
-    @lane = Arr.new(10)
-    @river = Arr.new(10)
+  attr_accessor :size, :safe_lane, :challenging_lanes 
+  def initialize(size)
+    @size = size
+    @safe_lane = Array.new(size) {".."}
+    @challenging_lanes = {
+          :road => {
+             :safe_symbol => " _",
+             :danger_symbol => "%%",
+          }, 
+          :river => {
+              :safe_symbol => "==",
+              :danger_symbol => "^^",
+            }
+    }        
   end  
   
-  def create_row(frequency, object)
-    Array.new(size*2) {rand(frequency) == 1 ? object : space}
+  def create_row(frequency, row)
+    Array.new(size) {rand(frequency) == 1 ? row[:danger_symbol] : row[:safe_symbol]}
   end  
-  
-  def tick_row(row, object, frequency, speed)
-    row.unshift(rand(frequency) == 1 ? object : space)
-    row.pop
-    sleep speed
-  end    
 end  
 
 
 class Board
-  attr_accessor :board, :size
-  def initialize(size)
+  attr_accessor :size, :lane, :safe_lane, :road, :river, :board
+  def initialize(size, difficulty)
     @size = size
-    @board = create_board
+    @lane = Lane.new(size)
+    @safe_lane = lane.safe_lane
+    @road = lane.create_row(difficulty, lane.challenging_lanes[:road])
+    @river = lane.create_row(difficulty, lane.challenging_lanes[:river])
+    @board = board_init
   end  
 
 
-  def create_board
-    Array.new(@size) {
-    if rand(3) == 1
-      rand(2) == 1 ? lane : river
-    else
-      safe_lane
+  def board_init 
+    board_arr = []
+    lane.safe_lane
+    3.times do
+      board_arr << safe_lane.dup
     end  
-    } 
+    number_of_hard_lanes = size-3
+    hard_lane_section = number_of_hard_lanes/2 - 2
+    hard_lane_section.times do 
+      board_arr << choose_hard_lane.dup
+    end 
+    3.times do 
+     board_arr << safe_lane.dup 
+    end 
+    hard_lane_section.times do 
+      board_arr << choose_hard_lane.dup
+    end  
+    3.times do 
+      board_arr << safe_lane.dup
+    end  
+    board_arr
+  end   
+
+  def choose_hard_lane
+    if rand(5) == 1 || rand(2) == 1 
+        road
+      else  
+        river
+      end  
   end  
 
   def draw_board
-    board.each_with_index do |row, y|
-      row.each_with_index do |col, x|
-        print "#{board[y][x]}"
-      end
-      puts ""  
-    end  
+    width = board.flatten.max.to_s.size+2
+    puts board.map { |a| a.map { |i| i.to_s.rjust(width) }.join}
+    puts ""
     puts "LIFE BAR GOES HERE"
+    sleep 0.1
+  end
+  
+  def tick_row(row, row_type)
+    row.unshift(rand(4) == 1 ? row_type[:danger_symbol] : row_type[:safe_symbol])
+    row.pop
   end
 
-  def tick_game(frequency1, frequency2)
+  def tick
+    draw_board
+    board.each do |row|
+      if [" _", "%%"].include?(row[0])
+        tick_row(row, lane.challenging_lanes[:road])
+      elsif ["==", "^^"].include?(row[0])
+        tick_row(row, lane.challenging_lanes[:river])
+      end  
+    end
+    sleep 0.15
   end  
-
 end  
 
 class Character
-  attr_accessor :image
-  def initialize
-    @image = "πø"
+  attr_accessor :player
+  def initialize(size)
+    @player = {
+          :position => {:y => -1, :x => (size/2)-1},
+          :image => "πø"
+    }  
   end 
 end  
 
-b = Board.new(20)
+b = Board.new(30, 4)
+c = Character.new(30)
+puts c.player[:position][:x]
 
-
-def play(game)
+def play(game, player)
   while true
-    game.draw_board
-    game.tick_game(3, 4)
-    puts 
+    game.board[(player.player[:position][:y])][(player.player[:position][:x])] = player.player[:image]
+    game.tick
   end
 end  
 
-play(b)
+play(b, c)
